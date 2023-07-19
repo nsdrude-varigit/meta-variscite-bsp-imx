90ab1eb9ad0c358e7e1b751188226dc8a49df428
Nate Drude <nate.d@variscite.com>
2023-07-19 16:44:41 -0500

    pm-utils: unload/load moal driver before/after suspend
    The moal driver is needed for wifi based on Murata LBES5PL2XX / NXP IW612.
    
    Suspend is not working while the moal driver is loaded, it will
    immediately resume.
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- recipes-bsp/pm-utils/pm-utils/02-wifi.sh
+++ recipes-bsp/pm-utils/pm-utils/02-wifi.sh
@@ -6,9 +6,14 @@ case $1 in
 
 "suspend")
         /etc/wifi/variscite-wifi stop
+        ifconfig mlan0 down;
+        ifconfig wfd0 down;
+        ifconfig uap0 down;
+        modprobe -r moal;
         ;;
 "resume")
         /etc/wifi/variscite-wifi start
+        modprobe moal
         if [ -f /etc/init.d/connman ]; then
                 killall -9 wpa_supplicant
                 /etc/init.d/connman restart
